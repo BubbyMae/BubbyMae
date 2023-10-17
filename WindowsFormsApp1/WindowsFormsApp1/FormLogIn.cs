201c6436b48ec4226061f18f928ec5fc72f0f2a8
BubbyMae <131413098+BubbyMae@users.noreply.github.com>
2023-10-17 18:21:36 +0800

    Add files via upload

---
+++ WindowsFormsApp1/WindowsFormsApp1/FormLogIn.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class FormLogIn : Form
+    {
+        Form1 fgrid;
+        public FormLogIn(Form1 fgrid)
+        {
+            InitializeComponent();
+            this.fgrid = fgrid;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            fgrid.dataGridView1.Rows.Add(textBoxName.Text, textBoxCourse.Text, textBoxYlvl.Text);
+        }
+    }
+}
